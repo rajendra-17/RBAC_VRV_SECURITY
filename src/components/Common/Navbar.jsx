63be864f259cbd3e1228e1873d68974f6126bed4
rajendra-17 <imrajendra2004@gmail.com>
2024-11-27 01:35:19 +0530

    Initial commit

---
+++ src/components/Common/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-[#001F3F] rounded-xl w-full text-white h-16 flex items-center justify-center px-4 shadow-md">
+      <h1 className="text-base tracking-wide sm:text-2xl font-bold text-center">
+        Role-Based Access Control (RBAC)
+      </h1>
+    </div>
+  );
+};
+
+export default Navbar;
